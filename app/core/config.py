505f0edf252eeda95e751acff0cf66a51e77f39d
cpiaseque <cpiaseque.work@gmail.com>
2026-01-30 17:53:25 +0300

    fix config

--- app/core/config.py
+++ app/core/config.py
@@ -1,19 +1,21 @@
 from pydantic import PostgresDsn, RedisDsn, SecretStr
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+from pathlib import Path
+
+
+DOTENV_PATH: Final = Path(__file__).parent.parent / ".env"
+
 class Settings(BaseSettings):
     TELEGRAM_ACCESS_TOKEN: SecretStr
 
     DB_DSN: PostgresDsn
 
-
-    REDIS_URL: RedisDsn
-
     # Business Logic
     BASIC_VALUE_BYN: int
 
     model_config = SettingsConfigDict(
-        env_file=".env",
+        env_file=str(DOTENV_PATH),
         env_file_encoding="utf-8",
         extra="ignore"
     )
